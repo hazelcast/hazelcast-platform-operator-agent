2899b15d7efb960b3c7c22b1340b481448ba64c6
Dominik Zeromski <dominik.zeromski@hazelcast.com>
2022-04-14 16:14:06 +0200

    [CN-306] Add naive restore implementation

--- restore.go
+++ restore.go
@@ -3,19 +3,22 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/google/subcommands"
 	"github.com/kelseyhightower/envconfig"
+	"gocloud.dev/blob"
+
+	_ "gocloud.dev/blob/s3blob"
 )
 
 type restoreCmd struct {
-	// credentials from secret
-	Region       string
-	AccessKey    string
-	AccessSecret string
-	// backup to restore
-	BucketPath string
+	Bucket      string
+	Destination string
 }
 
 func (*restoreCmd) Name() string     { return "restore" }
@@ -23,18 +26,70 @@ func (*restoreCmd) Synopsis() string { return "run restore agent" }
 func (*restoreCmd) Usage() string    { return "" }
 
 func (r *restoreCmd) SetFlags(f *flag.FlagSet) {
-	f.StringVar(&r.Region, "region", "", "bucket cloud region")
-	f.StringVar(&r.AccessKey, "key", "", "bucket access key")
-	f.StringVar(&r.AccessSecret, "secret", "", "bucket secret key")
-	f.StringVar(&r.BucketPath, "path", "", "bucket path")
+	f.StringVar(&r.Bucket, "src", "", "src bucket path")
+	f.StringVar(&r.Destination, "dst", "/data/persistence/backup", "dst filesystem path")
 }
 
-func (r *restoreCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+func (r *restoreCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	// overwrite config with environment variables
 	if err := envconfig.Process("restore", r); err != nil {
-		log.Fatalln(err)
-		return subcommands.ExitUsageError
+		log.Println(err)
+		return subcommands.ExitFailure
+	}
+
+	// run download process
+	if err := download(ctx, r.Bucket, r.Destination); err != nil {
+		log.Println(err)
+		return subcommands.ExitFailure
 	}
 
 	return subcommands.ExitSuccess
+}
+
+func download(ctx context.Context, src, dst string) error {
+	bucket, err := blob.OpenBucket(ctx, src)
+	if err != nil {
+		return err
+	}
+	defer bucket.Close()
+
+	iter := bucket.List(nil)
+	for {
+		obj, err := iter.Next(ctx)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		// naive validation, we only want zip files
+		if !strings.HasSuffix(obj.Key, ".zip") {
+			continue
+		}
+		if err := save(ctx, bucket, obj.Key, dst); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func save(ctx context.Context, bucket *blob.Bucket, key, path string) error {
+	s, err := bucket.NewReader(ctx, key, nil)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	d, err := os.Create(filepath.Join(path, key))
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	if _, err := io.Copy(d, s); err != nil {
+		return err
+	}
+
+	return nil
 }
