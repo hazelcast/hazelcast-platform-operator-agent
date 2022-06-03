560f8a1fb4fd43ad45db7f87e1923fa2ec9fa16a
Dominik Zeromski <dominik.zeromski@hazelcast.com>
2022-06-03 15:23:12 +0200

    [CN-413] Backup to bucket without temporary file

--- backup/backup.go
+++ backup/backup.go
@@ -1,12 +1,15 @@
 package backup
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-	"path"
+	"path/filepath"
+	"strings"
 
 	"gocloud.dev/blob"
 	"gocloud.dev/blob/gcsblob"
@@ -23,81 +26,89 @@ import (
 	"github.com/hazelcast/platform-operator-agent/util"
 )
 
-func UploadBackup(ctx context.Context, bucket *blob.Bucket, bucketURL, backupFolderPath, hazelcastCRName string) error {
-	logger := util.GetLogger(ctx)
-
-	backupFolderFileList, err := ioutil.ReadDir(backupFolderPath)
+func UploadBackup(ctx context.Context, bucket *blob.Bucket, bucketURL, backupsDir, prefix string) error {
+	backupSeqs, err := ioutil.ReadDir(backupsDir)
 	if err != nil {
-		logger.Errorf("Error occurred while read backup folder: %v", err)
-		return nil
+		return err
 	}
 
-	if len(backupFolderFileList) == 0 {
-		logger.Printf("Backup folder doesn't have files/folders. Backup folder: %s", backupFolderPath)
-		return nil
-	}
-
-	logger.Info("Uploading backup folders to the bucket...")
-
-	for _, bf := range backupFolderFileList {
-		backupItem := fmt.Sprintf("%s/%s", backupFolderPath, bf.Name())
-		UUIDFolderList, err := ioutil.ReadDir(backupItem)
+	// iterate over <backup-dir>/backup-<backupSeq>/ dirs
+	for _, s := range backupSeqs {
+		seqDir := filepath.Join(backupsDir, s.Name())
+		backupUUIDs, err := ioutil.ReadDir(seqDir)
 		if err != nil {
-			logger.Errorf("Error occurred while read backup folder for timestamp: %v", err)
-			return nil
+			return err
 		}
-		for _, uf := range UUIDFolderList {
-			zipFilePath := fmt.Sprintf("/%s/%s.zip", backupFolderPath, uf.Name())
-			UUIDBackupFolderPath := path.Join(backupFolderPath, bf.Name(), uf.Name())
 
-			if err := util.ZipFolder(UUIDBackupFolderPath, zipFilePath); err != nil {
-				logger.Errorf("Couldn't zip hot backup folder: %s. Err: %v", UUIDBackupFolderPath, err)
-				return nil
+		seq := util.ConvertHumanReadableFormat(s.Name())
+
+		// iterate over <backup-dir>/backup-<backupSeq>/<UUID> dirs
+		for _, u := range backupUUIDs {
+			uuidDir := filepath.Join(seqDir, u.Name())
+
+			key := filepath.Join(prefix, seq, u.Name()+".tar.gz")
+			if err := uploadBackup(ctx, bucket, key, uuidDir, u.Name()); err != nil {
+				return err
 			}
 
-			humanReadableBackupFolder := util.ConvertHumanReadableFormat(bf.Name())
-			err = uploadBackupToBucket(ctx, bucket, fmt.Sprintf("%s/%s/%s.zip", hazelcastCRName, humanReadableBackupFolder, uf.Name()), zipFilePath)
-			if err != nil {
-				logger.Errorf("Backup folder: %s couldn't be uploaded. Err: %v", humanReadableBackupFolder, err)
-				logger.Debugf("Backup folder: %s couldn't be uploaded. Err: %v", fmt.Sprintf("%s/%s", bf.Name(), uf.Name()), err)
-				return nil
+			// after uploading backup we can remove local copy
+			if err := os.RemoveAll(uuidDir); err != nil {
+				return err
 			}
-			logger.Infof("Backup folder: %s were succesfully uploaded to %s", humanReadableBackupFolder, bucketURL)
-			logger.Debugf("Backup folder: %s were succesfully uploaded to %s", fmt.Sprintf("%s/%s", bf.Name(), uf.Name()), bucketURL)
+		}
+
+		// we finished uploading backups
+		if err := os.RemoveAll(seqDir); err != nil {
+			return err
 		}
 	}
 
-	// Remove all files and folder under backup folder.
-	if err := util.RemoveAllContent(backupFolderPath); err != nil {
-		logger.Errorf("Backup folders couldn't be cleaned up. Err: %v", err)
-	}
-
-	logger.Infof("Backup folders were succesfully cleaned up from local.")
 	return nil
 }
 
-func uploadBackupToBucket(ctx context.Context, bucket *blob.Bucket, fileName string, filePath string) error {
-	defer bucket.Close()
-	if accessible, _ := bucket.IsAccessible(ctx); accessible {
-		w, err := bucket.NewWriter(context.Background(), fileName, nil)
-		if err != nil {
-			return fmt.Errorf("Could not create writer: %v", err)
-		}
-		defer w.Close()
-
-		src, err := os.Open(filePath)
-		if err != nil {
-			return fmt.Errorf("Could not open file: %v", err)
-		}
-
-		_, err = io.Copy(w, src)
-		if err != nil {
-			return fmt.Errorf("Failed to copy data: %v\n", err)
-		}
-	} else {
-		return fmt.Errorf("Bucket is not accesible.")
+func uploadBackup(ctx context.Context, bucket *blob.Bucket, name, backupDir, baseDir string) error {
+	w, err := bucket.NewWriter(ctx, name, nil)
+	if err != nil {
+		return err
 	}
-	return nil
+	defer w.Close()
+
+	g := gzip.NewWriter(w)
+	defer g.Close()
+
+	t := tar.NewWriter(g)
+	defer t.Close()
+
+	return filepath.Walk(backupDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(info, info.Name())
+		if err != nil {
+			return err
+		}
+
+		// make sure files are relative to baseDir
+		header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, backupDir))
+
+		if err := t.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(t, f)
+		return err
+	})
 }
 
 func OpenBucket(ctx context.Context, bucketURL, secretName string) (*blob.Bucket, error) {
