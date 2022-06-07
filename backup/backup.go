70597419e878828652ceddcec61896f389e79899
dzeromski-hazelcast <102033346+dzeromski-hazelcast@users.noreply.github.com>
2022-06-07 12:20:09 +0200

    [CN-413] Replace zip with tgz archive format in backup (#9)
    * [CN-413] Replace zip with tgz archive format in backup
    
    * [CN-413] Replace zip with tgz archive format in restore
    
    * [CN-413] Backup to bucket without temporary file
    
    * [CN-413] Add ErrEmptyBackupDir error

--- backup/backup.go
+++ backup/backup.go
@@ -1,181 +1,221 @@
 package backup
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-	"path"
+	"path/filepath"
+	"strings"
 
 	"gocloud.dev/blob"
-	_ "gocloud.dev/blob/azureblob"
 	"gocloud.dev/blob/gcsblob"
-	_ "gocloud.dev/blob/s3blob"
 	"gocloud.dev/gcp"
 	"golang.org/x/oauth2/google"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
+
+	_ "gocloud.dev/blob/azureblob"
+	_ "gocloud.dev/blob/s3blob"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"github.com/hazelcast/platform-operator-agent/util"
 )
 
-func UploadBackup(ctx context.Context, bucket *blob.Bucket, bucketURL, backupFolderPath, hazelcastCRName string) error {
-	backupFolderFileList, backupFolderErr := ioutil.ReadDir(backupFolderPath)
-	logger := util.GetLogger(ctx)
-	if backupFolderErr != nil {
-		logger.Errorf("Error occurred while read backup folder: %v", backupFolderErr)
-		return nil
+var ErrEmptyBackupDir = errors.New("empty backup directory")
+
+func UploadBackup(ctx context.Context, bucket *blob.Bucket, bucketURL, backupsDir, prefix string) error {
+	backupSeqs, err := ioutil.ReadDir(backupsDir)
+	if err != nil {
+		return err
 	}
 
-	if len(backupFolderFileList) == 0 {
-		logger.Printf("Backup folder doesn't have files/folders. Backup folder: %s", backupFolderFileList)
-		return nil
+	if len(backupSeqs) == 0 {
+		return ErrEmptyBackupDir
 	}
 
-	logger.Info("Uploading backup folders to the bucket...")
+	// iterate over <backup-dir>/backup-<backupSeq>/ dirs
+	for _, s := range backupSeqs {
+		seqDir := filepath.Join(backupsDir, s.Name())
+		backupUUIDs, err := ioutil.ReadDir(seqDir)
+		if err != nil {
+			return err
+		}
 
-	for _, bf := range backupFolderFileList {
-		backupItem := fmt.Sprintf("%s/%s", backupFolderPath, bf.Name())
-		UUIDFolderList, UUIDFolderErr := ioutil.ReadDir(backupItem)
-		if UUIDFolderErr != nil {
-			logger.Errorf("Error occurred while read backup folder for timestamp: %v", UUIDFolderErr)
+		seq := util.ConvertHumanReadableFormat(s.Name())
+
+		// iterate over <backup-dir>/backup-<backupSeq>/<UUID> dirs
+		for _, u := range backupUUIDs {
+			uuidDir := filepath.Join(seqDir, u.Name())
+
+			key := filepath.Join(prefix, seq, u.Name()+".tar.gz")
+			if err := uploadBackup(ctx, bucket, key, uuidDir, u.Name()); err != nil {
+				return err
+			}
+
+			// after uploading backup we can remove local copy
+			if err := os.RemoveAll(uuidDir); err != nil {
+				return err
+			}
+		}
+
+		// we finished uploading backups
+		if err := os.RemoveAll(seqDir); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func uploadBackup(ctx context.Context, bucket *blob.Bucket, name, backupDir, baseDir string) error {
+	w, err := bucket.NewWriter(ctx, name, nil)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	g := gzip.NewWriter(w)
+	defer g.Close()
+
+	t := tar.NewWriter(g)
+	defer t.Close()
+
+	return filepath.Walk(backupDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(info, info.Name())
+		if err != nil {
+			return err
+		}
+
+		// make sure files are relative to baseDir
+		header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, backupDir))
+
+		if err := t.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if info.IsDir() {
 			return nil
 		}
-		for _, uf := range UUIDFolderList {
-			zipFilePath := fmt.Sprintf("/%s/%s.zip", backupFolderPath, uf.Name())
-			UUIDBackupFolderPath := path.Join(backupFolderPath, bf.Name(), uf.Name())
-			backupZipErr := util.ZipFolder(UUIDBackupFolderPath, zipFilePath)
-			if backupZipErr != nil {
-				logger.Errorf("Couldn't zip hot backup folder: %s. Err: %v", UUIDBackupFolderPath, backupZipErr)
-				return nil
-			}
-			humanReadableBackupFolder := util.ConvertHumanReadableFormat(bf.Name())
-			uploadErr := uploadBackupToBucket(ctx, bucket, fmt.Sprintf("%s/%s/%s.zip", hazelcastCRName, humanReadableBackupFolder, uf.Name()), zipFilePath)
-			if uploadErr != nil {
-				logger.Errorf("Backup folder: %s couldn't be uploaded. Err: %v", humanReadableBackupFolder, uploadErr)
-				logger.Debugf("Backup folder: %s couldn't be uploaded. Err: %v", fmt.Sprintf("%s/%s", bf.Name(), uf.Name()), uploadErr)
-				return nil
-			}
-			logger.Infof("Backup folder: %s were succesfully uploaded to %s", humanReadableBackupFolder, bucketURL)
-			logger.Debugf("Backup folder: %s were succesfully uploaded to %s", fmt.Sprintf("%s/%s", bf.Name(), uf.Name()), bucketURL)
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
 		}
-	}
-	// Remove all files and folder under backup folder.
-	removeErr := util.RemoveAllContent(backupFolderPath)
-	if removeErr != nil {
-		logger.Errorf("Backup folders couldn't be cleaned up. Err: %v", removeErr)
-	}
-	logger.Infof("Backup folders were succesfully cleaned up from local.")
-	return nil
+		defer f.Close()
+
+		_, err = io.Copy(t, f)
+		return err
+	})
 }
 
-func uploadBackupToBucket(ctx context.Context, bucket *blob.Bucket, fileName string, filePath string) error {
-	defer bucket.Close()
-	if accessible, _ := bucket.IsAccessible(ctx); accessible {
-		w, err := bucket.NewWriter(context.Background(), fileName, nil)
-		if err != nil {
-			return fmt.Errorf("Could not create writer: %v", err)
-		}
-		defer w.Close()
-
-		src, err := os.Open(filePath)
-		if err != nil {
-			return fmt.Errorf("Could not open file: %v", err)
-		}
-
-		_, err = io.Copy(w, src)
-		if err != nil {
-			return fmt.Errorf("Failed to copy data: %v\n", err)
-		}
-	} else {
-		return fmt.Errorf("Bucket is not accesible.")
-	}
-	return nil
-}
-
-func GetBucket(ctx context.Context, bucketURL, secretName string) (*blob.Bucket, error) {
-	provider, bucketName, err := util.ValidateBucketURL(bucketURL)
+func OpenBucket(ctx context.Context, bucketURL, secretName string) (*blob.Bucket, error) {
+	provider, _, err := util.ValidateBucketURL(bucketURL)
 	if err != nil {
 		return nil, err
 	}
+
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		return nil, err
 	}
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
+
 	namespace, err := util.GetNamespace()
 	if err != nil {
 		return nil, err
 	}
+
 	secret, err := clientset.CoreV1().Secrets(namespace).Get(context.TODO(), secretName, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
+
 	switch provider {
 	case util.AWS:
-		if err := setCredentialEnv(secret.Data, util.BucketDataS3AccessKeyID, util.BucketDataS3EnvAccessKeyID, provider); err != nil {
-			return nil, err
-		}
-		if err := setCredentialEnv(secret.Data, util.BucketDataS3Region, util.BucketDataS3EnvRegion, provider); err != nil {
-			return nil, err
-		}
-		if err := setCredentialEnv(secret.Data, util.BucketDataS3SecretAccessKey, util.BucketDataS3EnvSecretAccessKey, provider); err != nil {
-			return nil, err
-		}
-		bucket, err := blob.OpenBucket(ctx, bucketURL)
-		if err != nil {
-			return nil, fmt.Errorf("could not open %s bucket %v", bucketURL, err)
-		}
-		return bucket, nil
-	case util.GCP:
-		credValue, ok := secret.Data[util.BucketDataGCPCredentialFile]
-		if !ok {
-			return nil, fmt.Errorf("invalid secret for %v : missing credential: %v", provider, util.BucketDataGCPCredentialFile)
-		}
-		creds, err := google.CredentialsFromJSON(ctx, credValue, "https://www.googleapis.com/auth/cloud-platform")
-		if err != nil {
-			return nil, err
-		}
-		client, err := gcp.NewHTTPClient(
-			gcp.DefaultTransport(),
-			gcp.CredentialsTokenSource(creds))
-		if err != nil {
-			return nil, err
-		}
+		return openAWS(ctx, bucketURL, secret.Data)
 
-		bucket, err := gcsblob.OpenBucket(ctx, client, bucketName, nil)
-		if err != nil {
-			return nil, fmt.Errorf("could not open %s bucket %v", bucketURL, err)
-		}
-		return bucket, nil
+	case util.GCP:
+		return openGCP(ctx, bucketURL, secret.Data)
+
 	case util.AZURE:
-		if err := setCredentialEnv(secret.Data, util.BucketDataAzureStorageAccount, util.BucketDataAzureEnvStorageAccount, provider); err != nil {
-			return nil, err
-		}
-		if err := setCredentialEnv(secret.Data, util.BucketDataAzureStorageKey, util.BucketDataAzureEnvStorageKey, provider); err != nil {
-			return nil, err
-		}
-		bucket, err := blob.OpenBucket(ctx, bucketURL)
-		if err != nil {
-			return nil, fmt.Errorf("could not open %s bucket %v", bucketURL, err)
-		}
-		return bucket, nil
+		return openAZURE(ctx, bucketURL, secret.Data)
+
 	default:
 		return nil, fmt.Errorf("invalid bucket path")
 	}
 }
 
-func setCredentialEnv(secretData map[string][]byte, credKey, credEnvKey, provider string) error {
-	credValue, ok := secretData[credKey]
-	if !ok {
-		return fmt.Errorf("invalid secret for %v : missing credential: %v", provider, credKey)
+func openAWS(ctx context.Context, bucketURL string, secret map[string][]byte) (*blob.Bucket, error) {
+	if err := setCredentialEnv(secret, util.BucketDataS3AccessKeyID, util.BucketDataS3EnvAccessKeyID); err != nil {
+		return nil, err
 	}
-	return os.Setenv(credEnvKey, string(credValue))
+	if err := setCredentialEnv(secret, util.BucketDataS3Region, util.BucketDataS3EnvRegion); err != nil {
+		return nil, err
+	}
+	if err := setCredentialEnv(secret, util.BucketDataS3SecretAccessKey, util.BucketDataS3EnvSecretAccessKey); err != nil {
+		return nil, err
+	}
+
+	return blob.OpenBucket(ctx, bucketURL)
+}
+
+func openGCP(ctx context.Context, bucketURL string, secret map[string][]byte) (*blob.Bucket, error) {
+	_, bucketName, err := util.ValidateBucketURL(bucketURL)
+	if err != nil {
+		return nil, err
+	}
+
+	value, ok := secret[util.BucketDataGCPCredentialFile]
+	if !ok {
+		return nil, fmt.Errorf("invalid secret for GCP : missing credential: %v", util.BucketDataGCPCredentialFile)
+	}
+
+	creds, err := google.CredentialsFromJSON(ctx, value, "https://www.googleapis.com/auth/cloud-platform")
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := gcp.NewHTTPClient(
+		gcp.DefaultTransport(),
+		gcp.CredentialsTokenSource(creds),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return gcsblob.OpenBucket(ctx, client, bucketName, nil)
+}
+
+func openAZURE(ctx context.Context, bucketURL string, secret map[string][]byte) (*blob.Bucket, error) {
+	if err := setCredentialEnv(secret, util.BucketDataAzureStorageAccount, util.BucketDataAzureEnvStorageAccount); err != nil {
+		return nil, err
+	}
+
+	if err := setCredentialEnv(secret, util.BucketDataAzureStorageKey, util.BucketDataAzureEnvStorageKey); err != nil {
+		return nil, err
+	}
+
+	return blob.OpenBucket(ctx, bucketURL)
+}
+
+func setCredentialEnv(secret map[string][]byte, key, name string) error {
+	value, ok := secret[key]
+	if !ok {
+		return fmt.Errorf("invalid secret: missing key: %v", key)
+	}
+	return os.Setenv(name, string(value))
 }
