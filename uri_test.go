e0d980b2176aaf743bbb20716297eb01813418a2
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-07-28 16:05:34 +0300

    Fix test case for TestBasicFormatURI

--- uri_test.go
+++ uri_test.go
@@ -14,7 +14,7 @@ func TestBasicFormatURI(t *testing.T) {
 		{"gs", "gs://bucket-name", "gs://bucket-name", false},
 		{"prefix", "s3://bucket-name/hazelcast", "s3://bucket-name?prefix=hazelcast/", false},
 		{"longprefix", "azblob://backup/hazelcast/2022-06-02-21-57-49/", "azblob://backup?prefix=hazelcast/2022-06-02-21-57-49/", false},
-		{"query", "s3://bucket-name/hazelcast?region=us-west-1", "s3://bucket-name?prefix=hazelcast/", false},
+		{"query", "s3://bucket-name/hazelcast?region=us-west-1", "s3://bucket-name?prefix=hazelcast/&region=us-west-1", false},
 		{"legacy", "s3://bucket-name?prefix=hazelcast/", "s3://bucket-name?prefix=hazelcast/", false},
 		{"duplicate", "s3://bucket-name/hazelcast??prefix=hazelcast", "s3://bucket-name?prefix=hazelcast/", false},
 	}
