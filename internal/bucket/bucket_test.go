c3041347450c00cc9756fa2fa0e7a9b18f0f66ce
Serge Bishyr <seriybg@gmail.com>
2023-04-21 14:52:22 +0300

    fix tests

--- internal/bucket/bucket_test.go
+++ internal/bucket/bucket_test.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"gocloud.dev/blob/memblob"
+
+	"github.com/hazelcast/platform-operator-agent/internal/fileutil"
 )
 
 func TestSaveFileFromBackup(t *testing.T) {
@@ -52,6 +54,95 @@ func TestSaveFileFromBackup(t *testing.T) {
 			file, err := os.ReadFile(filePath)
 			require.Nil(t, err)
 			require.Equal(t, []byte("content"), file)
+		})
+	}
+}
+
+func TestDownloadClassJars(t *testing.T) {
+	tests := []struct {
+		name          string
+		dstPathExists bool
+		files         []fileutil.File
+		wantFiles     []fileutil.File
+		wantErr       bool
+	}{
+		{
+			"only jar allowed",
+			true,
+			[]fileutil.File{
+				{Name: "file1"},
+				{Name: "test1.jar"},
+				{Name: "test2.class"},
+			},
+			[]fileutil.File{
+				{Name: "test1.jar"},
+			},
+			false,
+		},
+		{
+			"no subfolder jars allowed",
+			true,
+			[]fileutil.File{
+				{Name: "folder1/test2.jar"},
+				{Name: "test1.jar"},
+				{Name: "test2.jar"},
+			},
+			[]fileutil.File{
+				{Name: "test1.jar"},
+				{Name: "test2.jar"},
+			},
+			false,
+		},
+		{
+			"no jar",
+			true,
+			[]fileutil.File{
+				{Name: "folder1/test2.jar"},
+				{Name: "test1.jar2"},
+				{Name: "jarjar"},
+			},
+			[]fileutil.File{},
+			false,
+		},
+		{
+			"dest path does not exist",
+			false,
+			[]fileutil.File{
+				{Name: "test1.jar"},
+			},
+			[]fileutil.File{},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Prepare the buckets and destination folder
+			tmpdir, err := os.MkdirTemp("", "download_class_jars")
+			require.Nil(t, err)
+			defer os.RemoveAll(tmpdir)
+
+			bucketPath := path.Join(tmpdir, "bucket")
+			err = fileutil.CreateFiles(bucketPath, tt.files, true)
+			require.Nil(t, err)
+
+			var dstPath string
+			if tt.dstPathExists {
+				dstPath, err = os.MkdirTemp(tmpdir, "dest")
+				require.Nil(t, err, "Destination Path could not be created")
+			} else {
+				dstPath = path.Join(tmpdir, "dest-does-not-exist")
+			}
+
+			// Run the tests
+			err = DownloadClassJars(context.Background(), "file://"+bucketPath, dstPath, nil)
+			require.Equal(t, tt.wantErr, err != nil, "Error is: ", err)
+			if err != nil {
+				require.Contains(t, err.Error(), "no such file or directory")
+				return
+			}
+			copiedFiles, err := fileutil.DirFileList(dstPath)
+			require.Nil(t, err)
+			require.ElementsMatch(t, copiedFiles, tt.wantFiles)
 		})
 	}
 }
