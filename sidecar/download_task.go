b8fc164312e6f2fadc7868bbb670f612417ed67a
dzeromski-hazelcast <102033346+dzeromski-hazelcast@users.noreply.github.com>
2024-04-09 10:22:17 +0200

    [CN-1131] Add UserCodeNamespace CR (#71)
    * [CN-1131] Add UserCodeNamespace CR
    
    * refactor
    
    * refactor
    
    ---------
    
    Co-authored-by: Serge Bishyr <seriybg@gmail.com>

--- sidecar/download_task.go
+++ sidecar/download_task.go
@@ -1,8 +1,15 @@
 package sidecar
 
 import (
+	"archive/zip"
 	"context"
 	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+
+	"gocloud.dev/blob"
 
 	"github.com/hazelcast/platform-operator-agent/internal/bucket"
 	"github.com/hazelcast/platform-operator-agent/internal/fileutil"
@@ -34,4 +41,64 @@ func downloadFromBucket(ctx context.Context, req DownloadFileReq) error {
 
 func downloadFromUrl(ctx context.Context, req DownloadFileReq) error {
 	return fileutil.DownloadFileFromURL(ctx, req.URL, req.DestDir)
+}
+
+func downloadBundle(ctx context.Context, req BundleReq) error {
+	secretData, err := bucket.SecretData(ctx, req.SecretName)
+	if err != nil {
+		return err
+	}
+	bucketURI, err := uri.NormalizeURI(req.URL)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(filepath.Clean(req.DestDir))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	b, err := bucket.OpenBucket(ctx, bucketURI, secretData)
+	if err != nil {
+		return err
+	}
+	defer b.Close()
+
+	w := zip.NewWriter(f)
+	iter := b.List(nil)
+	for {
+		obj, err := iter.Next(ctx)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		// we only want top level files and no files under sub-folders
+		if path.Base(obj.Key) != obj.Key {
+			continue
+		}
+		if err = readZip(ctx, b, obj, w); err != nil {
+			return err
+		}
+	}
+	return w.Close()
+}
+
+func readZip(ctx context.Context, b *blob.Bucket, obj *blob.ListObject, w *zip.Writer) error {
+	r, err := b.NewReader(ctx, obj.Key, nil)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	f, err := w.Create(obj.Key)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+	return nil
 }
